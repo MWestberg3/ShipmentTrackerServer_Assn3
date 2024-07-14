b4f0e4afe3ea303eecfd888cd359e6da5c917da7
Michael Westberg <mwestberg@bamboohr.com>
2024-07-14 13:01:19 -0600

    updating shipment class

--- src/main/kotlin/Shipment.kt
+++ src/main/kotlin/Shipment.kt
@@ -1,2 +1,22 @@
-class Shipment {
+class Shipment(
+    var status: String,
+    var id: String,
+    notes: MutableList<String>,
+    updateHistory: MutableList<ShippingUpdate>,
+    var expectedDeliveryDate: Long,
+    var currentLocation: String) {
+
+    var notes: MutableList<String> = notes
+        private set
+
+    var updateHistory: MutableList<ShippingUpdate> = updateHistory
+        private set
+
+    fun addNote(note: String) {
+        notes.add(note)
+    }
+
+    fun addUpdate(update: ShippingUpdate) {
+        updateHistory.add(update)
+    }
 }
