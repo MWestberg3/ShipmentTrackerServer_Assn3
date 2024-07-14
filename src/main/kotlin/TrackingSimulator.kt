f7298d0745ccd1736c045aa574cfae0b4e52ed66
Michael Westberg <mwestberg@bamboohr.com>
2024-07-14 13:46:15 -0600

    adding simulator skeleton

--- src/main/kotlin/TrackingSimulator.kt
+++ src/main/kotlin/TrackingSimulator.kt
@@ -1,2 +1,10 @@
-class TrackingSimulator {
+class TrackingSimulator(private var shipments: MutableList<Shipment>) {
+    fun findShipmentById(id: String): Shipment? {
+        return shipments.find { it.id == id }
+    }
+    fun addShipment(shipment: Shipment) {
+        shipments.add(shipment)
+    }
+    fun runSimulation() {
+    }
 }
