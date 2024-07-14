b5b761df9df8d45516fd730933cc7573a8ee54a2
Michael Westberg <mwestberg@bamboohr.com>
2024-07-14 14:23:00 -0600

    more or less working code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
@@ -25,15 +26,63 @@ import kotlinx.coroutines.runBlocking
 @Preview
 fun App() {
     val trackerViewHelper = remember { TrackerViewHelper() }
-    var text by remember { mutableStateOf("Hello, World!") }
+    val coroutineScope = rememberCoroutineScope()
+    val shipments = loadShipmentData()
+    val trackingSimulator = TrackingSimulator(shipments)
+    var shipmentIdInput by remember { mutableStateOf(TextFieldValue("")) }
+    var searchResult by remember { mutableStateOf<String?>(null)}
 
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+    MaterialTheme{
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+                    .background(Color.White),
+            ) {
+                TextField(value = shipmentIdInput,
+                    onValueChange = { shipmentIdInput = it },
+                    label = { Text("Enter shipment ID") })
+                Button(onClick = {
+                    coroutineScope.launch {
+                        val found = trackingSimulator.findShipmentById(shipmentIdInput.text)
+                        searchResult = if (found != null) "Shipment found!" else "Shipment not found!"
+                    }
+                }) {
+                    Text("Look up Shipment ID")
+                }
+                if (searchResult != null) {
+                    Text(searchResult!!)
+                }
+            }
         }
     }
+}
+
+fun loadShipmentData(): MutableList<Shipment> {
+    val shipment1 = Shipment(
+        "In transit",
+        "1",
+        mutableListOf("Note 1", "Note 2"),
+        mutableListOf(ShippingUpdate("In transit", "Location 1", 1652712855468)),
+        1626345600,
+        "Location 1"
+    )
+    val shipment2 = Shipment(
+        "Delivered",
+        "2",
+        mutableListOf("Note 1", "Note 2"),
+        mutableListOf(ShippingUpdate("In transit", "Location 1", 1652712855468)),
+        1626345600,
+        "Location 1"
+    )
+    return mutableListOf(shipment1, shipment2)
 }
 
 fun main() = application {
