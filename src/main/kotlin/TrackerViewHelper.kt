7b651db72bdb8d93014e786af51e54efe8dc9751
Michael Westberg <mwestberg@bamboohr.com>
2024-07-14 13:45:19 -0600

    updated viewhelper

--- src/main/kotlin/TrackerViewHelper.kt
+++ src/main/kotlin/TrackerViewHelper.kt
@@ -1,2 +1,23 @@
-class TrackerViewHelper {
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+class TrackerViewHelper() {
+    var shipmentId by mutableStateOf("")
+        private set
+    var shipmentTotes by mutableStateOf(mutableListOf<String>())
+        private set
+    var shipmentUpdateHistory by mutableStateOf(mutableListOf<String>())
+        private set
+    var expectedShipmentDeliveryDate by mutableStateOf(mutableListOf<String>())
+        private set
+    var shipmentStatus by mutableStateOf("")
+        private set
+
+    fun trackShipment(id: String) {
+
+    }
+    fun stopTracking() {
+
+    }
 }
